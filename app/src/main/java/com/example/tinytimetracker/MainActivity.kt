a5ee0f8c2e8cb994220d8554a953d2633f4f3e17
Mirco Hölzenbein <mircosgiga@hotmail.de>
2021-02-18 12:17:50 +0100

    added & implemented new main_activity layout also redefined the trackinghistoryicon

--- app/src/main/java/com/example/tinytimetracker/MainActivity.kt
+++ app/src/main/java/com/example/tinytimetracker/MainActivity.kt
@@ -2,18 +2,26 @@ package com.example.tinytimetracker
 
 import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.view.View
-import android.widget.EditText
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageButton
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w: Window = window
+            w.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
 
         // get reference to button
         val btn_click_me = findViewById(R.id.imageButton) as ImageButton
